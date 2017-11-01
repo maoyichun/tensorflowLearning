7028828aea84c1a3672d41cf0ddfecab57812ded
mao <gunnermao@163.com>
2017-11-01 13:42:59 +0800

    add notes_02.py

--- cs20si/notes_02.py
+++ cs20si/notes_02.py
@@ -101,6 +101,18 @@ import tensorflow as tf
 a = tf.placeholder(tf.float32, shape=[3])
 b = tf.constant([5, 5, 5], tf.float32)
 c = a + b
+list_of_a_value = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 with tf.Session() as sess:
     writer = tf.summary.FileWriter('./my_graph', sess.graph)
-    print(sess.run(c, feed_dict={a: [1, 2, 3]}))
+    for a_value in list_of_a_value:
+        print(sess.run(c, feed_dict={a: a_value}))
+
+x = tf.constant([[1, 2, 3], [2, 3, 4]])
+print(tf.Graph.is_feedable(tf.get_default_graph(), x))
+
+x = tf.add(2, 5)
+y = tf.multiply(x, 3)
+
+with tf.Session() as sess:
+    replace_dict = {x: 15}
+    print(sess.run(y, feed_dict=replace_dict))
